VasekPurchart/Tracy-Blue-Screen-Bundle

bundle skeleton

src/DependencyInjection/Configuration.php
@@ -1,0 +1,18 @@
+<?php
+
+namespace VasekPurchart\TracyBlueScreenBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
+class Configuration implements \Symfony\Component\Config\Definition\ConfigurationInterface
+{
+
+	/**
+	 * @return \Symfony\Component\Config\Definition\Builder\TreeBuilder
+	 */
+	public function getConfigTreeBuilder()
+	{
+		return new TreeBuilder();
+	}
+
+}

VasekPurchart/Doctrine-Date-Time-Immutable-Types-Bundle

src/DependencyInjection/Configuration.php
@@ -1,0 +1,18 @@
+<?php
+
+namespace VasekPurchart\DoctrineDateTimeImmutableTypesBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+
+class Configuration implements \Symfony\Component\Config\Definition\ConfigurationInterface
+{
+
+	/**
+	 * @return \Symfony\Component\Config\Definition\Builder\TreeBuilder
+	 */
+	public function getConfigTreeBuilder()
+	{
+		return new TreeBuilder();
+	}
+
+}